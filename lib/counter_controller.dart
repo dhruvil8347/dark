d6e6a94bf98d0f27bca9fb249f4d24292806ea12
vivek <vivek@euqitysoft.in>
2023-06-21 12:49:25 +0530

    h

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,6 +1,40 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class CounterController extends GetxController{
-  RxInt counter=0.obs;
+class CounterController extends GetxController {
+  RxInt counter = 0.obs;
+  RxString Username = "".obs;
+  final TextEditingController name = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    counter.value = 0;
+    Username.value = "";
+  }
+
+
+
+  void plusCount() {
+    counter.value++;
+  }
+
+  void minusCount() {
+    counter.value--;
+  }
 
 }
+
+/*class Namecontroller extends GetxController{
+  RxString Username = "".obs;
+
+@override
+  void onInit() {
+    super.onInit();
+    Username.value;
+  }
+
+  void username(){
+  Username.obs;
+  }
+}*/
--- lib/setting.dart
+++ lib/setting.dart
@@ -1,14 +1,92 @@
+import 'package:dark/counter_controller.dart';
 import 'package:flutter/material.dart';
-class Setting extends StatefulWidget {
-  const Setting({Key? key}) : super(key: key);
+import 'package:get/get.dart';
 
-  @override
-  State<Setting> createState() => _SettingState();
-}
+class Setting extends StatelessWidget {
+  final CounterController c = Get.put(CounterController());
 
-class _SettingState extends State<Setting> {
+
+  Setting({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    c.plusCount();
+                  },
+                  child: const Text("+")),
+              SizedBox(
+                width: 50,
+              ),
+              Obx(() {
+                return Text(c.counter.value.toString());
+              }),
+              SizedBox(
+                width: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    c.minusCount();
+                  },
+                  child: const Text("-")),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 1.0,
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0))
+                ]),
+            child: Obx(() {
+              return TextField(
+                controller: c.name,
+                onChanged: (value) => c.Username,
+                decoration: InputDecoration(
+                  hintText: c.Username.value,
+                  border: InputBorder.none,
+                ),
+              );
+            }),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                c.Username.value=c.name.text.toString();
+              },
+              child: Text("print")),
+          SizedBox(
+            height: 10,
+          ),
+          Obx(() {
+            return Text(
+              c.Username.value.toString(),
+              style: TextStyle(color: Colors.black),
+            );
+          })
+        ],
+      ),
+    );
   }
 }
