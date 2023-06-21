330336f44de3269691e5cf0927f343d08a9e8ac9
vivek <vivek@euqitysoft.in>
2023-06-21 15:01:10 +0530

    h

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -4,16 +4,26 @@ import 'package:get/get.dart';
 class CounterController extends GetxController {
   RxInt counter = 0.obs;
   RxString Username = "".obs;
+
+
   final TextEditingController name = TextEditingController();
+
+  final TextEditingController num1 = TextEditingController();
+  final TextEditingController num2 = TextEditingController();
+
+
 
   @override
   void onInit() {
     super.onInit();
     counter.value = 0;
     Username.value = "";
+
   }
 
-
+  void setText() {
+    setText();
+  }
 
   void plusCount() {
     counter.value++;
--- lib/setting.dart
+++ lib/setting.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 class Setting extends StatelessWidget {
   final CounterController c = Get.put(CounterController());
 
+  //final Namecontroller n = Get.put(Namecontroller());
 
   Setting({
     Key? key,
@@ -71,6 +72,7 @@ class Setting extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
+
           ElevatedButton(
               onPressed: () {
                 c.Username.value=c.name.text.toString();
@@ -79,12 +81,81 @@ class Setting extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
+
           Obx(() {
             return Text(
               c.Username.value.toString(),
               style: TextStyle(color: Colors.black),
             );
-          })
+          }),
+        /*  SizedBox(
+            height: 20,
+          ),
+
+      TextField(
+        controller: c.name,
+        onChanged: (value) => c.Username,
+        decoration: InputDecoration(
+          hintText: c.Username.value,
+          border: InputBorder.none,
+        ),
+      )*/
+          Text("calculater"),
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                blurRadius: 1.0,
+                color: Colors.grey,
+                offset: Offset(0.0, 0.0))
+              ]
+            ),
+            child: TextField(
+              controller: c.num1,
+              decoration: InputDecoration(
+                hintText: "number1",
+                border: InputBorder.none,
+
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            height: 50,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 1.0,
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0))
+                ]
+            ),
+            child: TextField(
+              controller: c.num2,
+              decoration: InputDecoration(
+                hintText: "number2",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+           SizedBox(
+             height: 20,
+           ),
+          ElevatedButton(onPressed: (){
+            c.num1; c.num2;
+          }, child: Text("calculate")),
+          SizedBox(
+            height: 20,
+          ),
+          Text("result"),
+
         ],
       ),
     );
